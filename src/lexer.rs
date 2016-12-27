1a50460e201043bcc04182bd5a3206b2d4d23486
Travis B. Hartwell <nafai@travishartwell.net>
2016-12-26 20:25:58 -0700

    Match better expresses the intent

--- src/lexer.rs
+++ src/lexer.rs
@@ -47,75 +47,77 @@ pub fn tokenize_line(line: &str) -> Result<LineOfCode, String> {
                 return Err(format!("Line must start with a line number:\n\t{}", line));
             }
         } else {
-            if ch.is_whitespace() {
-                // Skip whitespace
-                continue;
-            }
-
-            // At the beginning of a string
-            if ch == '"' {
-                // TODO: Handle escaped quotes
-                // TODO: Handle malformed string
-                let str_chars: Vec<char> = char_iter.by_ref()
-                    .take_while(|&(_, x)| x != '"')
-                    .map(|(_, x)| x)
-                    .collect();
-                let bstring: String = str_chars.into_iter().collect();
-                tokens.push(TokenAndPos(pos, token::Token::BString(bstring)))
-            } else if ch == '-' {
-                if !tokens.is_empty() && tokens.last().unwrap().1.is_value() {
-                    tokens.push(TokenAndPos(pos, token::Token::Minus))
-                } else {
-                    tokens.push(TokenAndPos(pos, token::Token::UMinus))
+            match ch {
+                ch if ch.is_whitespace() => {
+                    // Skip whitespace
+                    continue;
                 }
-            } else if ch == '!' {
-                // Unary operators aren't necessarily separated by whitespace
-                tokens.push(TokenAndPos(pos, token::Token::Bang))
-            } else if ch == '(' {
-                tokens.push(TokenAndPos(pos, token::Token::LParen))
-            } else if ch == ')' {
-                tokens.push(TokenAndPos(pos, token::Token::RParen))
-            } else {
-                // Otherwise, next token is until next whitespace
-                let mut token_chars: Vec<char> = char_iter.by_ref()
-                    .peeking_take_while(|&(_, x)| !x.is_whitespace() || x == ')')
-                    .map(|(_, x)| x)
-                    .collect();
-                token_chars.insert(0, ch);
-                let token_str: String = token_chars.into_iter().collect();
 
-                if i32::from_str(token_str.as_str()).is_ok() {
-                    tokens.push(TokenAndPos(pos,
+                // At the beginning of a string
+                '"' => {
+                    // TODO: Handle escaped quotes
+                    // TODO: Handle malformed string
+                    let str_chars: Vec<char> = char_iter.by_ref()
+                        .take_while(|&(_, x)| x != '"')
+                        .map(|(_, x)| x)
+                        .collect();
+                    let bstring: String = str_chars.into_iter().collect();
+                    tokens.push(TokenAndPos(pos, token::Token::BString(bstring)))
+                }
+                '-' => {
+                    if !tokens.is_empty() && tokens.last().unwrap().1.is_value() {
+                        tokens.push(TokenAndPos(pos, token::Token::Minus))
+                    } else {
+                        tokens.push(TokenAndPos(pos, token::Token::UMinus))
+                    }
+                }
+                '!' => tokens.push(TokenAndPos(pos, token::Token::Bang)),
+                '(' => tokens.push(TokenAndPos(pos, token::Token::LParen)),
+                ')' => tokens.push(TokenAndPos(pos, token::Token::RParen)),
+                _ => {
+                    // Otherwise, next token is until next whitespace or closing paren
+                    let mut token_chars: Vec<char> = char_iter.by_ref()
+                        .peeking_take_while(|&(_, x)| !x.is_whitespace() || x == ')')
+                        .map(|(_, x)| x)
+                        .collect();
+                    token_chars.insert(0, ch);
+                    let token_str: String = token_chars.into_iter().collect();
+
+                    if i32::from_str(token_str.as_str()).is_ok() {
+                        tokens.push(TokenAndPos(pos,
                                           token::Token::Number(i32::from_str(token_str.as_str())
                                               .unwrap())));
-                } else {
-                    let token = token::Token::token_for_string(token_str.as_str());
+                    } else {
+                        let token = token::Token::token_for_string(token_str.as_str());
 
-                    match token {
-                        None => {
-                            if is_valid_identifier(&token_str) {
-                                tokens
+                        match token {
+                            None => {
+                                if is_valid_identifier(&token_str) {
+                                    tokens
                                     .push(TokenAndPos(pos,
                                                       token::Token::Variable(token_str
                                                                              .to_string())))
-                            } else {
-                                return Err(format!("Unimplemented token at {}:\t{}",
-                                                   pos,
-                                                   token_str));
+                                } else {
+                                    return Err(format!("Unimplemented token at {}:\t{}",
+                                                       pos,
+                                                       token_str));
+                                }
                             }
-                        }
-                        Some(token::Token::Rem) => {
-                            tokens.push(TokenAndPos(pos, token::Token::Rem));
-                            // Skip the space after REM
-                            char_iter.next();
-                            // The rest of the line is a comment
-                            let comment_str: String = char_iter.by_ref().map(|(_, x)| x).collect();
-                            tokens.push(TokenAndPos((pos + 4) as u32,
-                                                    token::Token::Comment(comment_str)))
-                        }
 
-                        Some(token) => {
-                            tokens.push(TokenAndPos(pos, token));
+                            Some(token::Token::Rem) => {
+                                tokens.push(TokenAndPos(pos, token::Token::Rem));
+                                // Skip the space after REM
+                                char_iter.next();
+                                // The rest of the line is a comment
+                                let comment_str: String =
+                                    char_iter.by_ref().map(|(_, x)| x).collect();
+                                tokens.push(TokenAndPos((pos + 4) as u32,
+                                                        token::Token::Comment(comment_str)))
+                            }
+
+                            Some(token) => {
+                                tokens.push(TokenAndPos(pos, token));
+                            }
                         }
                     }
                 }
@@ -155,7 +157,6 @@ fn is_valid_identifier(token_str: &str) -> bool {
 #[cfg(test)]
 mod tests {
     use lexer::*;
-    use token::*;
 
     #[test]
     fn tokenize_no_line_number() {
