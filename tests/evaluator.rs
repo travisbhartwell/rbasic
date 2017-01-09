fc164c5314ec2585862ca78e72aaf201b10a5116
Travis B. Hartwell <nafai@travishartwell.net>
2017-01-09 10:18:32 -0700

    Why repeat code even in tests?

--- tests/evaluator.rs
+++ tests/evaluator.rs
@@ -3,42 +3,37 @@ extern crate rbasic;
 use rbasic::lexer::*;
 use rbasic::evaluator::*;
 
-#[test]
-fn eval_goto_invalid_target_line_number() {
-    let result = tokenize_line("10 GOTO 5");
+fn eval_line(line: &str) -> Result<String, String> {
+    let result = tokenize_line(line);
     assert!(result.is_ok());
     let code_line = result.unwrap();
-    let eval_result = evaluate(vec![code_line]).err();
+    evaluate(vec![code_line])
+}
+
+#[test]
+fn eval_goto_invalid_target_line_number() {
+    let eval_result = eval_line("10 GOTO 5").err();
     assert_eq!(eval_result,
                Some("At LineNumber(10), 8 invalid target line for GOTO".to_string()));
 }
 
 #[test]
 fn eval_goto_invalid_line_number() {
-    let result = tokenize_line("10 GOTO A");
-    assert!(result.is_ok());
-    let code_line = result.unwrap();
-    let eval_result = evaluate(vec![code_line]).err();
+    let eval_result = eval_line("10 GOTO A").err();
     assert_eq!(eval_result,
                Some("At LineNumber(10), 8 GOTO must be followed by valid line number".to_string()));
 }
 
 #[test]
 fn eval_goto_no_line_number() {
-    let result = tokenize_line("10 GOTO");
-    assert!(result.is_ok());
-    let code_line = result.unwrap();
-    let eval_result = evaluate(vec![code_line]).err();
+    let eval_result = eval_line("10 GOTO").err();
     assert_eq!(eval_result,
                Some("At LineNumber(10), 7 GOTO must be followed by a line number".to_string()));
 }
 
 #[test]
 fn eval_input_no_variable() {
-    let result = tokenize_line("10 INPUT");
-    assert!(result.is_ok());
-    let code_line = result.unwrap();
-    let eval_result = evaluate(vec![code_line]).err();
+    let eval_result = eval_line("10 INPUT").err();
     assert_eq!(eval_result,
                Some("At LineNumber(10), 8 INPUT must be followed by a variable name".to_string()));
 }
